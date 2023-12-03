4dcfe855a27fac079031c05e03941cf55028c0a3
davidpaz0283 <151471495+davidpaz0283@users.noreply.github.com>
2023-12-03 16:17:58 -0300

    feat: add rectangle class

---
+++ Recuperatorio_Parcial/src/Rectangle.java
@@ -0,0 +1,47 @@
+public class Rectangle extends Shape{
+    private double width = 1.0;
+    private double length = 1.0;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    protected Rectangle(double width, double length, String color, boolean filled) {
+        this.width = width;
+        this.length = length;
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getArea() {
+        return width * length;
+    }
+
+    public double getPerimeter() {
+        return 2 * (width + length);
+    }
+
+    public String toString() {
+        return "Rectangle [ Shape [\ncolor:  + color +  filled:  + filled ]  width:  + width +  length:  + length]";
+    }
+}
