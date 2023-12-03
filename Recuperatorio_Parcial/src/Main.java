6993fc049eef043a274edd9af94afe61bda1b6db
davidpaz0283 <151471495+davidpaz0283@users.noreply.github.com>
2023-12-03 16:31:39 -0300

    feat: add main class

--- Recuperatorio_Parcial/src/Main.java
+++ Recuperatorio_Parcial/src/Main.java
@@ -1,17 +1,26 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Shape shaObject1 = new Circle(5);
+        Shape shaObject2 = new Rectangle(5, 3);
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("The two objects have the same area? " +
+                equalArea(shaObject1, shaObject2));
 
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        displayGeometricObject(shaObject1);
+
+        displayGeometricObject(shaObject2);
+    }
+
+
+    public static boolean equalArea(Shape shaObject1, Shape shaObject2) {
+        return shaObject1.getArea() == shaObject2.getArea();
+    }
+
+    public static void displayGeometricObject(Shape object) {
+        System.out.println();
+        System.out.println("The area is " + object.getArea());
+        System.out.println("The perimeter is " + object.getPerimeter());
     }
 }
+
+
